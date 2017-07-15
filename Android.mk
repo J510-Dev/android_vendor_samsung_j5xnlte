1fb4e3b01c9eb9d448da761cdefb902c037e7deb
GeneticEnginer <igor_cestari@hotmail.com>
2017-07-15 03:40:56 -0300

    Revert "update gps/time blobs"
    This reverts commit 210add7e3b66974d5d93323585c41b756bb1ea4e.

--- Android.mk
+++ Android.mk
@@ -6,36 +6,33 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
-LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_PREBUILT)
 
 ######################
 ### libloc_api_v02
 include $(CLEAR_VARS)
 LOCAL_MODULE       := libloc_api_v02
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES    := proprietary/lib/libloc_api_v02.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_OWNER := qcom
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES    := proprietary/lib/libloc_api_v02.so
 include $(BUILD_PREBUILT)
 
 ######################
 
-### libloc_ds_api
+### libloc_eng
 include $(CLEAR_VARS)
-LOCAL_MODULE := libloc_ds_api
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/lib/libloc_ds_api.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE       := libloc_eng
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_OWNER := qcom
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES    := proprietary/lib/libloc_eng.so
 include $(BUILD_PREBUILT)
 
 #######################
@@ -43,28 +40,15 @@ include $(BUILD_PREBUILT)
 ### libHevcSwDecoder
 include $(CLEAR_VARS)
 LOCAL_MODULE       := libHevcSwDecoder
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_OWNER := qcom
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_SRC_FILES    := proprietary/vendor/lib/libHevcSwDecoder.so
 include $(BUILD_PREBUILT)
 
 #######################
-
-### TimeService
-include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
 
 endif
 
